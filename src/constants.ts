a5824a51088891f6f978c0de8b431e6b3a2f2f53
eoparra <edgarparramadrigal@gmail.com>
2026-02-19 17:06:33 -0600

    Update ADMIN_PASSWORD to 'dishadmin'

--- src/constants.ts
+++ src/constants.ts
@@ -4,5 +4,5 @@ export const CATEGORIES: CategoryName[] = ['Fruit shop', 'Butchery', 'Supermarke
 
 export const MAX_SELECTED_DISHES = 10;
 
-export const ADMIN_PASSWORD = 'admin1234'; // change this to your preferred password
+export const ADMIN_PASSWORD = 'dishadmin'; // change this to your preferred password
 export const ADMIN_AUTH_STORAGE_KEY = 'dish-manager-admin-auth';
